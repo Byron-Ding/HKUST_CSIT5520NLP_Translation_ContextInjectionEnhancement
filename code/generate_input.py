f1907c98b4dc1c35a5c1e02527d94cca6c99ec59
Byron Ding <84700771+Byron-Ding@users.noreply.github.com>
2026-04-16 20:47:46 +0800

    add new data

--- code/generate_input.py
+++ code/generate_input.py
@@ -5,14 +5,17 @@ import yaml
 import importlib.util
 import importlib.machinery
 import re
+import regex
 from typing import Optional
 import aiofiles
 
-# re.DOTALL with \n
-result_format_regex: re.Pattern = re.compile(r"<output>(.*?)</output>", re.DOTALL)
+# regex.DOTALL with \n
+result_format_regex: regex.Pattern[str] = regex.compile(r"<output>(.*?)</output>", regex.DOTALL)
+# 匹配所有汉字（基本区块 + 扩展区 + 兼容汉字）
+all_chinese_characters: regex.Pattern[str] = regex.compile(r'[\u4e00-\u9fff\u3400-\u4dbf\uf900-\ufaff\u20000-\u2a6df\u2a700-\u2b73f\u2b740-\u2b81f\u2b820-\u2ceaf\u2ceb0-\u2ebef]')
 
 # A JSON schema for a book
-schema = {
+schema: dict[str, dict[str, dict[str, str]] | str] = {
     "type": "object",
     "properties": {
         "only_one_translate_with_comment": {
@@ -26,6 +29,7 @@ schema = {
 
 # lmstudio_root_path: pathlib.Path = pathlib.Path(r"F:/.lmstudio/lmstudio-community/")
 
+# ----------------------------------------------- Model Paths ------------------------------------------------
 meta_llama_path: str = r"meta-llama-3.1-8b-instruct"
 qwen_path: str = r"qwen/qwen3.5-9b"
 bytedance_seed_oss_36b_path: str = r"bytedance/seed-oss-36b"
@@ -37,6 +41,7 @@ bytedance_seed_oss_36b_path: str = r"bytedance/seed-oss-36b"
 
 # print(result)
 
+# ----------------------------------------------- Project Paths ------------------------------------------------
 # set .. as the root path
 project_root_path: pathlib.Path = pathlib.Path(__file__).parent.parent
 
@@ -45,22 +50,30 @@ data_path = project_root_path / "data" / r"config" / "test.yaml"
 # store data
 output_data_base_path = project_root_path / "output"
 
-with open(data_path, "r", encoding="utf-8") as f:
-    data = yaml.safe_load(f)
-
-
 # general template
 # import from /data/dataclass/template/general_translate.py
 general_translate_module_path = project_root_path / "data" / "dataclass" / "template" / "general_translate.py"
 
+
+
+# ------------------------------------------------ Load Data ------------------------------------------------
+with open(data_path, "r", encoding="utf-8") as f:
+    data = yaml.safe_load(f)
+
+# ------------------------------------------------ Load General Template ------------------------------------------------
 spec: Optional[importlib.machinery.ModuleSpec] = importlib.util.spec_from_file_location("general_translate", general_translate_module_path)
 
 if spec is not None and spec.loader is not None:
     general_template_module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(general_template_module)
+
     
 general_template: general_translate_module.GeneralTemplate = general_template_module.GeneralTemplate()
 
+
+
+
+# ------------------------------------------------ AI test ------------------------------------------------
 async def generate_input(
     model_path: str = qwen_path
 ):
@@ -78,8 +91,20 @@ async def generate_input(
             if each_sentence_input_language != "en":
                 # print(f"Input language {each_sentence_input_language} is not supported yet.")
                 continue
-
+                
             for each_keyword in each_sentence_keywords:
+                # ---------------- Output path ----------------
+                current_output_original_data_path: pathlib.Path = output_data_base_path / \
+                    model_path / \
+                    each_sentence_summary / \
+                    f"{each_keyword}_original.txt"
+                current_output_data_path: pathlib.Path = output_data_base_path / \
+                    model_path / \
+                    each_sentence_summary / \
+                    f"{each_keyword}.txt"
+                current_output_data_path.parent.mkdir(parents=True, exist_ok=True)
+                
+                # ---------- Generate input  ----------
                 each_sentence = each_sentence_template.format(
                     keyword = each_keyword
                 )
@@ -98,26 +123,28 @@ async def generate_input(
                     input_key_synonyms=synonyms_hint
                 )
                 
+                # [Debug] Print the full template to check if it's correct
                 # print(model.get_load_config())
-                
                 print(f"Input sentence: {full_template}")
+                
+                # ----------------------- Get response and stream it -----------------------
                 result = await model.respond_stream(
                     full_template
                 )
-                # Stream the response
-                async for fragment in result:
-                    print(fragment.content, end="", flush=True)
+                
+                with open(current_output_original_data_path, "w", encoding="utf-8") as f:
+                    # Stream the response
+                    async for fragment in result:
+                        f.write(fragment.content)
+                        print(fragment.content, end="", flush=True)
+                print()
                 print()
                 # Note that even for structured responses, the *fragment* contents are still only text
                 # Get the final structured result
-                result_result = result.result()
+                complete_result = result.result()
+                print(f"Output result: {complete_result}")
                 
-                print(f"Output result: {result_result}")
-                
-                current_output_data_path = output_data_base_path / model_path / each_sentence_summary / f"{each_keyword}.txt"
-                current_output_data_path.parent.mkdir(parents=True, exist_ok=True)
-                
-                real_result = extract_translate_with_comment_from_result(str(result))
+                real_result = extract_translate_with_comment_from_result(str(complete_result))
                 
                 if real_result is None:
                     print(f"No valid output found for sentence: {each_sentence}")
@@ -132,8 +159,10 @@ def extract_translate_with_comment_from_result(
 ) -> Optional[str]:
     # get all chinese characters in result
     # remove all english characters, punctuation and numbers
-    chinese_characters = re.sub(r"[a-zA-Z0-9\<\>\[\]\(\)\{\}\s\n\r]", "", result)
-    return chinese_characters if chinese_characters else None
+    chinese_characters = regex.findall(all_chinese_characters, result)
+    chinese_characters_str = " ".join(chinese_characters)
+    
+    return chinese_characters_str if chinese_characters_str else None
         
 if __name__ == "__main__":
     import asyncio
